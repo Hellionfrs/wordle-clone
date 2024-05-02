efb7191c38da4beed475b3e1d5fdc2ed257c37bb
fred <fredy.rodriguez@pucp.pe>
2024-05-02 11:53:05 -0500

    feat: styling app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,8 +3,8 @@ import Wordle from "../Wordle";
 function App() {
   return (
     <div className="flex justify-center justify-self-center ">
-      <div className="m-auto ">
-        <h1 className="text-3xl font-bold underline text-center">Wordle Clone</h1>
+      <div className="flex flex-col gap-2 m-auto mt-10">
+        <h1 className="text-4xl font-bold  text-center">Wordle Clone</h1>
         <Wordle />
       </div>
     </div>
